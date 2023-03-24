e8c9ce78c38972fc0851d2c3a08a4bfcf56ed9a8
Patrick Torgerson <TorgersonPatrickS@gmail.com>
2023-03-24 16:22:31 -0700

    Fix bright color macros not working

--- examples/bench.zig
+++ examples/bench.zig
@@ -9,16 +9,31 @@ const zcon = @import("zcon");
 
 pub fn main() !void {
     var out = zcon.Writer.init();
-
     defer out.flush();
     defer out.useDefaultColors();
 
-    out.putRaw("\n\n\n");
-    out.setForeground(zcon.Color.hex("a2a") orelse zcon.Color.col16(.red));
+    const evens = [_]i32{ 2, 4, 6, 8 };
+    const odds = [_]i32{ 1, 3, 5, 7 };
 
-    out.put("#box:21,3    Hello friend!");
-    out.cursorDown(5);
-    out.put("#down:5 #left:20 hello#indent friend");
-    out.put("#up:5 (printed last)");
-    out.put("#down:5\n one#red; two");
+    // new zig 0.11.x feature
+    for (&evens, &odds) |even, odd| {
+        out.fmt("{}, {}, ", .{ odd, even });
+    }
+    out.putChar('\n');
+
+    // sundenly for loops are usefull
+    // inclusive..exclusive
+    for (1..11) |i| {
+        out.fmt("({}) ", .{i});
+    }
+    out.putChar('\n');
+
+    out.put("\n \x1b[31m RED \x1b[91m BRED \n");
+    out.fmt("\n \x1b[{}m RED \x1b[{}m BRED \n", .{ 31, 91 });
+    out.put("\n #red; RED #bred; BRED \n\n");
+
+    out.setForeground(zcon.Color.col16(.red));
+    out.putRaw("  RED");
+    out.setForeground(zcon.Color.col16(.bright_red));
+    out.putRaw("  BRED\n\n");
 }
